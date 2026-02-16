497457ddc7af7e52aec215a44182adfac960a783
Sina Moghtader Far <Moghtaderfar@gmail.com>
2026-02-16 23:46:56 +0100

    Initial setup

---
+++ setup.bat
@@ -0,0 +1,22 @@
+@echo off
+
+mkdir apps
+mkdir static
+mkdir media
+mkdir templates
+mkdir utils
+mkdir media\uploads
+mkdir media\outputs
+
+
+mkdir templates\includes
+mkdir templates\tools\image_tools
+mkdir templates\tools\document_tools
+
+
+mkdir static\css
+mkdir static\js
+mkdir static\images
+
+
+pause
