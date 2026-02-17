156640a062ecf5ea98f58c267f06127ff27694f4
Sina Moghtader Far <Moghtaderfar@gmail.com>
2026-02-17 23:07:49 +0100

    Created tree structure in set.txt & Added Navbar, Footer, and Home page templates & Fixed and organized config settings & Added .gitignore file

---
+++ apps/pdf_tool/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class PdfToolConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.pdf_tool' 
--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,16 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -37,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'apps.core',
+    'apps.pdf_tool',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +62,10 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / 'templates',          
+            BASE_DIR / 'templates/includes', 
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
